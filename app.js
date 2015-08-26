dd060388cceb129b3810ea8ccb2b41188350d2e8
rag594 <raghavrastogi594@gmail.com>
2015-08-26 07:57:36 +0530

    removed favicon

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(multer({
 }));
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
