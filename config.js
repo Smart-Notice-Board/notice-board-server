e7ce67b9d8e9045983260d5cc441a31f919549b0
rag594 <raghavrastogi594@gmail.com>
2015-08-30 23:26:23 +0530

    added preloader

--- config.js
+++ config.js
@@ -2,21 +2,21 @@
  * Created by raghavrastogi on 07/07/15.
  */
 module.exports = {
-    /*mysql: {
+    mysql: {
         host: "localhost",
         port: 8889,
         user: "root",
         password: "root",
         database: "digitalnoticeboard"
     },
-    'secret': 'RAGHAV'*/
-    mysql: {
+    'secret': 'RAGHAV'
+    /*mysql: {
         host: "sql6.freemysqlhosting.net",
         port: 3306,
         user: "sql688441",
         password: "iP3!gL1*",
         database: "sql688441"
     },
-    'secret': 'RAGHAV'
+    'secret': 'RAGHAV'*/
 
 }
